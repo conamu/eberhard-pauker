8b64bd3fc4ebd60b7ad90a154bb98e83076175d1
Constantin Amundsen <constantin.amundsen@gmail.com>
2021-02-06 22:18:35 +0100

    added rudimentary help menu

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,12 @@
 from definitions import *
 from discord.ext import commands
+import discord
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='#', help_command=None)
 
 ha_records = open(HA_RECORDS, "w+")
 ha_records_archive = open(HA_RECORDS_ARCHIVE, "w+")
@@ -17,25 +18,45 @@ async def eintragen(ctx, fach, link, termin):
     eintr(fach, link, termin)
     await ctx.send("Ich habe die Hausaufgabe erfolgreich abgespeichert!  :notebook_with_decorative_cover:")
 
+
 @bot.command()
 async def ha(ctx):
     # Command to get all pending Homework assignments.
     await ctx.send(hausaufgaben())
+
 
 @bot.command()
 async def löschen(ctx, line):
     # Command to delete a Homework assignment from the List.
     await ctx.send(loesch(line))
 
+
 @bot.command()
 async def abstimmung(ctx, *args):
     # Command to create a poll.
     await ctx.send(abstimmen(*args))
 
+
 @bot.command()
 async def archiv(ctx):
     # Comman to view the Archive of old Assignments.
     await ctx.send(showArchive())
+
+
+@bot.command()
+async def hilfe(ctx):
+    # Display Help Menu.
+    await ctx.send(
+        embed=discord.Embed(title="Eberhard Pauker | Hilfe", description="", color=0x00ff00)
+        .add_field(name="#eintragen 'FACH' 'Link zur Aufgabe' 'Abgabetermin'", value="Abgelaufene Abgabetermine werden automatisch Archiviert.", inline=True)
+        .add_field(name="#ha", value="Eine numerierte Liste der noch Anstehenden Hausaufgaben.", inline=True)
+        .add_field(name="#löschen 'Nummer'", value="Lösche den Eintrag an der gewünschten Stelle.", inline=True)
+        .add_field(name="#archiv", value="Eine Liste aus allen bisherigen, abgelaufenen Aufgaben.", inline=True)
+        .add_field(name="#abstimmung 'Mehrere Themen/Stichpunkte'", value="Erstelle eine Abstimmung. "
+                                                                          "Mit einem Thema ist es eine Ja/Nein Abstimmung,"
+                                                                          "mehr Themen ergeben eine klassische Abstimmung", inline=True)
+        .set_footer(text="Hilfemenü für Dr. Dr. Prof. Eberhard Pauker", icon_url="")
+        .set_author(name="conamu | https://gitlab.ho-me.zone/conamu/", url="https://gitlab.ho-me.zone/conamu", icon_url=""))
 
 
 @bot.event
@@ -48,5 +69,6 @@ async def on_ready():
         f'{bot.user} is connected to the following servers:\n'
         f'{server.name}(id: {server.id})'
     )
+
 
 bot.run(TOKEN)
